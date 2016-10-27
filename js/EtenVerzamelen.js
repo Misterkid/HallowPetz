18c420d291ea9995a7c4ccd05aed87ccc0edd7fa
jackie <jacky.elzinga@gmail.com>
2016-10-27 12:33:04 +0200

    etenverzamelen

---
+++ js/EtenVerzamelen.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Moi on 10/26/2016.
+ */
+
+class EtenVerzamelen extends THREE.Mesh{
+
+    constructor(geometry, material)
+    {
+        super(geometry, material);
+
+
+
+    }
+    OnClick(e)
+    {
+
+    }
+
+}
